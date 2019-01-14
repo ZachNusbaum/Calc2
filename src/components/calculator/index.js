961aff8c548d2ca38bc5ec45f695c911bbc63ef6
Zach Nusbaum <zach@zdnmail.com>
2019-01-14 13:49:31 -0800

    Add HTML for calculator and some basic styling

---
+++ src/components/calculator/index.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+export class Calculator extends React.Component {
+  render() {
+    return <>
+      <div className="calculator">
+        <input type="text" class="calculator-screen" value="0" disabled />
+
+        <div className="calculator-keys">
+          <button type="button" className="operator" value="+">+</button>
+          <button type="button" className="operator" value="-">-</button>
+          <button type="button" className="operator" value="*">*</button>
+          <button type="button" className="operator" value="/">/</button>
+
+          <button type="button" value="7">7</button>
+          <button type="button" value="8">8</button>
+          <button type="button" value="7">9</button>
+
+          <button type="button" value="4">4</button>
+          <button type="button" value="5">5</button>
+          <button type="button" value="6">6</button>
+
+          <button type="button" value="1">1</button>
+          <button type="button" value="2">2</button>
+          <button type="button" value="3">3</button>
+
+          <button type="button" value="0">0</button>
+          <button type="button" className="decimal" value=".">.</button>
+          <button type="button" className="all-clear" value="all-clear">AC</button>
+
+          <button className="equal-sign" value="=">=</button>
+
+        </div>
+      </div>
+    </>
+  }
+}
